64f601261ae8e8c5a83ee36ea11e65c149072545
Preston <mprestonsparks@gmail.com>
2019-04-15 23:54:09 -0500

    fixed itemID choice in inquirer

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -8,50 +8,52 @@ let connection = mysql.createConnection({
     database: 'bamazon_db'
 });
 
-// displayProducts();
+// itemIds = [];
+function getInfo() {
+    connection.connect();
+     var queryString = 'SELECT * FROM products';
+     connection.query(queryString, function(err, rows, fields) {
+        if (err) throw err;
+        productIDs = [];
+        for (var i in rows) {
+            productIDs.push(rows[i].item_id);
+        };
+        // console.log(itemIds);
+        runInquirer(productIDs);
+    });
+    connection.end();
+};
 
-connection.connect();
- var queryString = 'SELECT * FROM products';
- connection.query(queryString, function(err, rows, fields) {
-    if (err) throw err;
-     for (var i in rows) {
-        console.log('Item ID: ', rows[i].item_id);
+getInfo();
+// console.log(itemIds.length);
+
+
+function runInquirer(productIDs) {
+inquirer
+  .prompt([
+    {
+        type: "list",
+        choices: productIDs,
+        name: "item_IDs",
+        message: "Select a query type"
     }
-});
-connection.end();
+])
+.then((answers) => {
+    itemID = answers.item_IDs;
+    console.log(productIDs);
+    console.log("Selected ItemID: " + itemID);
+
+  });
+};
 
 
-// var purchase_itemID;
-// var purchase_quantity;
-// inquirer
-//   .prompt([
-//     {
-//         type: "list",
-//         choices: {purchase_itemID},
-//         name: "purchase_itemID",
-//         message: "Select a product"
-//     },
-//     {
-//         type: "input",
-//         name: "purchase_quantity",
-//         message: "Enter the quantity to purchase"
-//     }
-//   ])
-//   .then((answers) => {
-//     //   console.log(answers.query_type, "...", answers.query_type2);
-//       userQuery = answers.userQuery;
-//       command = answers.command;
-//       run();
-//   });
-
-// function displayProducts() {
-//     let sql = `SELECT products.item_id, products.product_name, products.sale_price FROM products`;
-//     connection.query(sql, (error, results) => {
-//       if (error) {
-//         return console.error(error.message);
-//       }
-//       console.log(results);
-//     });
-//     connection.end(); 
-// };
-
+function displayProducts() {
+    let sql = `SELECT products.item_id, products.product_name, products.sale_price FROM products`;
+    connection.query(sql, (error, results) => {
+      if (error) {
+        return console.error(error.message);
+      }
+      console.log(results);
+    });
+    connection.end(); 
+};
